e1fbb5485359124f1245b513151448b455664bfc
mayankprocreates <mayysingh39@gmail.com>
2025-10-05 13:27:26 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -52,7 +52,7 @@ const Navigation = () => {
             >
               <div className="text-2xl">🌍</div>
               <span className="font-bold text-lg bg-gradient-to-r from-cosmic-cyan to-cosmic-blue bg-clip-text text-transparent">
-                Earth Whispers
+                Beneath Our Sky 🌎
               </span>
             </div>
 
